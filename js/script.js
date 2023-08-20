4d86e8c0f32d07d2dcefdd11f30e051abd667dfe
Dima <dikrasnikov@gmail.com>
2023-08-20 16:10:35 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// const obj = {
+//   name: "john",
+//   age: 25,
+//   isMarried: false,
+// };
+
+// console.log(obj["age"]);
+
+// const answer = prompt("Вам есть 39?", "18");
+// console.log(typeof answer);
+let answer = [];
+console.log(typeof answer);
